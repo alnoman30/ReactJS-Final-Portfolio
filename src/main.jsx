8d1e2804738e58531cc5fb3546be44a270503dff
Noman <alnomaan30@gmail.com>
2025-10-05 18:43:38 +0600

    Navbar complete

---
+++ src/main.jsx
@@ -0,0 +1,48 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home/Home.jsx';
+import About from './pages/about/About.jsx';
+import Service from './pages/service/Service.jsx';
+import Resume from './pages/resume/Resume.jsx';
+import Project from './pages/project/Project.jsx';
+import Contact from './pages/contact/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/service",
+        element: <Service/>,
+      },
+      {
+        path: "/resume",
+        element: <Resume/>,
+      },
+      {
+        path: "/project",
+        element: <Project/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
