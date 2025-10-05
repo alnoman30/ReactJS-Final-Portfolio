822bcc2fedf68113ae5ed45b5beafdb1673d631e
Noman <alnomaan30@gmail.com>
2025-10-05 22:57:13 +0600

    hero section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import vector1 from "../../assets/vector1.svg?url";
 import { Star } from "lucide-react";
 
 const Hero = () => {
@@ -15,11 +14,8 @@ const Hero = () => {
             <p className="border border-b-gray-600 inline-block px-6 py-3 rounded-[35px] text-secondary font-medium text-lg">
               Hello!
             </p>
-            <img
-              src={vector1}
-              alt="vector 1"
-              className="absolute -top-7 -right-8 w-10"
-            />
+            <img src="/vector1.svg" alt="vector 1" className="absolute -top-7 -right-8 w-10" />
+            
           </div>
           <h2 className="font-urban font-semibold text-[36px] sm:text-[56px] md:text-[72px] lg:text-[96px] leading-none mt-8">
             I’m <span className="text-primary">{personName}</span>, <br />
