b7b945ef0c95f3fcd2ea7a716a91ee7b64d4dcb2
Noman <alnomaan30@gmail.com>
2025-10-05 22:54:59 +0600

    hero section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -15,7 +15,11 @@ const Hero = () => {
             <p className="border border-b-gray-600 inline-block px-6 py-3 rounded-[35px] text-secondary font-medium text-lg">
               Hello!
             </p>
-            <img src={vector1} alt="vector 1" />
+            <img
+              src={vector1}
+              alt="vector 1"
+              className="absolute -top-7 -right-8 w-10"
+            />
           </div>
           <h2 className="font-urban font-semibold text-[36px] sm:text-[56px] md:text-[72px] lg:text-[96px] leading-none mt-8">
             I’m <span className="text-primary">{personName}</span>, <br />
